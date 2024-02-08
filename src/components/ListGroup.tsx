eaec4222314dcbfa65dde2c31480ad592be35084
Draven <valdevisor@gmail.com>
2024-02-08 23:41:32 +0800

    Update the ListGroupComponent

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,10 +4,10 @@ function ListGroup() {
       <li className="list-group-item active" aria-current="true">
         An active item
       </li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
+      <li className="list-group-item">item one </li>
+      <li className="list-group-item">item two</li>
+      <li className="list-group-item">item three</li>
+      <li className="list-group-item">item four</li>
     </ul>
   );
 }
