967a985a8d8987f6ac7a3db181582419cee2ebf8
Draven <valdevisor@gmail.com>
2024-02-10 18:09:21 +0800

    add handling events in ListGroup components

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,24 @@
 function ListGroup() {
   const items = ["Antipolo", "Masinag", "Marikina", "Robinson", "Sta.Lu"];
+  // items = [];
+
+  // const getMessage = () => {
+  //   return ;
+  // };
 
   return (
     <>
       <h1>This is a List</h1>
+      {/* {items.length === 0 ? <p>No Item Found</p> : null} */}
+      {items.length === 0 && <p>No Item Found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li
+            className="list-group-item"
+            onClick={(event) => console.log(event)}
+            key={item}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
